af9129cf62c48926dcd5572bb1b4a84d4a2167ae
maopademiao <865324539@qq.com>
2021-03-22 19:32:01 +0800

    update

--- generate_all_noisy_data_for_PEER.sh
+++ generate_all_noisy_data_for_PEER.sh
@@ -16,6 +16,16 @@ python datanoisy.py --rate 0.2  --dataset trec --result_dir noisy_data --type wh
 python datanoisy.py --rate 0.4  --dataset trec --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
 python datanoisy.py --rate 0.6  --dataset trec --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
 
+python datanoisy.py --rate 0.2  --dataset trec --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset trec --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset trec --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.2  --dataset trec --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset trec --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset trec --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.2  --dataset trec --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset trec --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset trec --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+
 # agnews
 python datanoisy.py --rate 0.2  --dataset agnews --result_dir noisy_data --type random --seed 0 --random_state 0 --peer
 python datanoisy.py --rate 0.4  --dataset agnews --result_dir noisy_data --type random --seed 0 --random_state 0 --peer
@@ -32,6 +42,16 @@ python datanoisy.py --rate 0.6  --dataset agnews --result_dir noisy_data --type 
 python datanoisy.py --rate 0.2  --dataset agnews --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
 python datanoisy.py --rate 0.4  --dataset agnews --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
 python datanoisy.py --rate 0.6  --dataset agnews --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
+
+python datanoisy.py --rate 0.2  --dataset agnews --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset agnews --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset agnews --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.2  --dataset agnews --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset agnews --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset agnews --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.2  --dataset agnews --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset agnews --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset agnews --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
 
 # chn
 python datanoisy.py --rate 0.2  --dataset chn --result_dir noisy_data --type random --seed 0 --random_state 0 --peer
@@ -50,6 +70,16 @@ python datanoisy.py --rate 0.2  --dataset chn --result_dir noisy_data --type whi
 python datanoisy.py --rate 0.4  --dataset chn --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
 python datanoisy.py --rate 0.6  --dataset chn --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
 
+python datanoisy.py --rate 0.2  --dataset chn --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset chn --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset chn --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.2  --dataset chn --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset chn --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset chn --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.2  --dataset chn --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset chn --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset chn --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+
 # chngolden
 python datanoisy.py --rate 0.2  --dataset chngolden --result_dir noisy_data --type random --seed 0 --random_state 0 --peer
 python datanoisy.py --rate 0.4  --dataset chngolden --result_dir noisy_data --type random --seed 0 --random_state 0 --peer
@@ -66,3 +96,13 @@ python datanoisy.py --rate 0.6  --dataset chngolden --result_dir noisy_data --ty
 python datanoisy.py --rate 0.2  --dataset chngolden --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
 python datanoisy.py --rate 0.4  --dataset chngolden --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
 python datanoisy.py --rate 0.6  --dataset chngolden --result_dir noisy_data --type white --seed 0 --random_state 0 --peer
+
+python datanoisy.py --rate 0.2  --dataset chngolden --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset chngolden --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset chngolden --result_dir noisy_data --multitype u_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.2  --dataset chngolden --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset chngolden --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset chngolden --result_dir noisy_data --multitype u_0.5_s_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.2  --dataset chngolden --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.4  --dataset chngolden --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
+python datanoisy.py --rate 0.6  --dataset chngolden --result_dir noisy_data --multitype s_0.5_p_0.5 --seed 0 --random_state 0 --peer
